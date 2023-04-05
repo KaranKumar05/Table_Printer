90ad316e1aff837832205d8aab2e622d6ca750b2
KARAN KUMAR <kkvmalhi@gmail.com>
2023-04-05 09:38:21 +0500

    js updeated

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function printTable() {
     let table = "<table>";
     table += "<tr><th>Multiplication Table of: " + number + "</th></tr>";
     let numbertill = document.getElementById("numbertill").value;
-    for (let i = 1; i <= numbertill; i++) {
+    for (i = 1; i <= numbertill; i++) {
         table += "<tr><td>" + number + " x " + i + " = " + (i * number) + "</td></tr>";
     }
     document.getElementById("tableContainer").innerHTML = table;
