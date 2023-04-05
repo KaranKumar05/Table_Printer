e86be85e9ebc3b2f98029c873d606e0ccdb52362
KARAN KUMAR <kkvmalhi@gmail.com>
2023-04-05 09:20:40 +0500

    first push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function printTable() {
+    let number = document.getElementById("numberInput").value;
+    let table = "<table>";
+    table += "<tr><th>Multiplication Table of: " + number + "</th></tr>";
+    let numbertill = document.getElementById("numbertill").value;
+    for (let i = 1; i <= numbertill; i++) {
+        table += "<tr><td>" + number + " x " + i + " = " + (i * number) + "</td></tr>";
+    }
+    document.getElementById("tableContainer").innerHTML = table;
+}
